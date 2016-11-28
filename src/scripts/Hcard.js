db2299e17db0591d78017bb3a9f73fb2226720d5
Catherine Jones <cath@bitgirl101.com>
2016-11-28 18:58:12 +1100

    add mobile gride classes

--- src/scripts/Hcard.js
+++ src/scripts/Hcard.js
@@ -6,13 +6,13 @@ class Hcard extends React.Component {
       <article className="vcard card">
         <div className="container card-heading-group">
           <div className="row">
-            <div className="col-sm-9">
+            <div className="col-sm-9 col-xs-8">
               <h2 className="fn">
                 <span className="given-name">Sam</span>
                 <span className="family-name">Fairfax</span>
               </h2>
             </div>
-            <div className="col-sm-3">
+            <div className="col-sm-3 col-xs-4">
               <div className="photo">
                 <img src="./assets/images/avatar.png" alt="avatar" />
               </div>
@@ -21,47 +21,48 @@ class Hcard extends React.Component {
         </div>
         <address className="card-body">
           <div className="underline row">
-            <div className="col-sm-3">
+            <div className="col-sm-3 col-xs-4">
               <span className="title">Email: </span>
             </div>
-            <div className="col-sm-9">
+            <div className="col-sm-9 col-xs-8">
               <span className="email">sam.fairfax@fairfax.com</span>
             </div>
           </div>
           <div className="underline row">
-            <div className="col-sm-3">
+            <div className="col-sm-3 col-xs-4">
               <span className="title">Phone: </span>
             </div>
-            <div className="col-sm-9">
+            <div className="col-sm-9 col-xs-8">
               <span className="phone">0987656736</span>
             </div>
           </div>
           <div className="adr">
             <div className="underline row">
-              <div className="col-sm-3">
+              <div className="col-sm-3 col-xs-4">
                 <span className="title">Address: </span>
               </div>
-              <div className="col-sm-9">
+              <div className="col-sm-9 col-xs-8">
                 <span className="street-address">1 Darling Island road</span>
               </div>
             </div>
             <div className="underline row">
-              <div className="offset-sm-3 col-sm-9">
+              <div className="offset-sm-3 col-sm-9 offset-xs-4 col-xs-8">
                 <span className="locality">Pyrmont </span>
                 <span className="region">NSW</span>
               </div>
             </div>
             <div className="underline row">
-              <div className="col-sm-3">
+              <div className="col-sm-3 col-xs-4">
                 <span className="title">Post Code: </span>
               </div>
-              <div className="col-sm-2">
+              <div className="col-sm-2 col-xs-8">
                 <span className="postal-code">2009</span>
               </div>
-              <div className="col-sm-3">
+
+              <div className="col-sm-3 col-xs-4">
                 <span className="title">Country: </span>
               </div>
-              <div className="col-sm-4">
+              <div className="col-sm-4 col-xs-8">
                 <span className="country-name">Australia</span>
               </div>
             </div>
