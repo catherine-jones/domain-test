473c34466c25ddb6c8990dce541b702338d03a7d
Catherine Jones <cath@bitgirl101.com>
2016-11-28 13:42:41 +1100

    first pass at sass

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,8 +1,8 @@
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import HcardForm from './HcardForm';
 import Hcard from './Hcard';
+import style from '../styles/main.scss';
 
 
 ReactDOM.render(<HcardForm/>, document.getElementById('form-container'));
