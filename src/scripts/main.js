c865cb75fdc62cd0333fa3ea2d17801e5eb1297d
Catherine Jones <cath@bitgirl101.com>
2016-11-29 05:38:39 +1100

    create a new component to contain exisitng

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ComponentContainer from './ComponentContainer';
 import HcardForm from './HcardForm';
 import Hcard from './Hcard';
 import style from '../styles/main.scss';
 
+ReactDOM.render(<ComponenetContainer/>, document.getElementById('app'));
+// ReactDOM.render(<HcardForm/>, document.getElementById('form-container'));
+// ReactDOM.render(<Hcard/>, document.getElementById('card-container'));
 
 ReactDOM.render(<HcardForm/>, document.getElementById('form-container'));
 ReactDOM.render(<Hcard/>, document.getElementById('card-container'));
