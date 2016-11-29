29101d3c6a718db4074b2cc80be65ee27ce7a23a
Catherine Jones <cath@bitgirl101.com>
2016-11-29 16:36:06 +1100

    fix typo on component name

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,9 +5,7 @@ import HcardForm from './HcardForm';
 import Hcard from './Hcard';
 import style from '../styles/main.scss';
 
-ReactDOM.render(<ComponenetContainer/>, document.getElementById('app'));
-// ReactDOM.render(<HcardForm/>, document.getElementById('form-container'));
-// ReactDOM.render(<Hcard/>, document.getElementById('card-container'));
+ReactDOM.render(<ComponentContainer/>, document.getElementById('app'));
 
 ReactDOM.render(<HcardForm/>, document.getElementById('form-container'));
 ReactDOM.render(<Hcard/>, document.getElementById('card-container'));
